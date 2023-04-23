012a9f5e406a0b4972fa775d2a6fef86e9e664da
João Vitor Alves Muniz <joao.vitor@akamaetech.com>
2023-04-23 09:49:18 -0300

    adicionar tags correta

--- src/components/Input/InputIcon.vue
+++ src/components/Input/InputIcon.vue
@@ -1,36 +1,37 @@
 <template>
   <div class="inputContainer">
-    <fa :icon="props.icon?.iconFont" class="iconeEmail" size="2x" color="red" />
-    <label class="placeholder"> {{ props.icon?.labelText }}</label>
-    <input type="text" id="txtEmail"/>
+    <fa :icon="props.property?.iconFont" class="iconeEmail" size="2x" :color="props.property?.iconColor" />
+    <label class="placeholder"> {{ props.property?.labelText }}</label>
+    <input type="text" id="txtEmail" />
   </div>
 </template>
 <style src="./InputIcon.css"></style>
 
 <script setup lang="ts">
-  import { defineProps as definePropsVue, PropType } from 'vue';
+import { defineProps as definePropsVue, PropType } from 'vue';
 
-  interface iconProps {
-    labelText: string | null,
-    iconFont: Array<string | null>
-  }
-    
+interface props {
+  labelText: string | null,
+  iconFont: Array<string | null>
+  iconColor: string | null
+}
+
+const props = defineProps({
+  property: Object as PropType<props>
+})
+
+/*
+ 
+  O código de cima está fazendo isso por baixo dos panos, por causa do TS.
+
+  const props = defineProps({...})
+
   const props = defineProps({
-    icon: Object as PropType<iconProps>
+    icon: {
+      type: Object as () => iconProps
+    }
   })
-
-  /*
-  
-    O código de cima está fazendo isso por baixo dos panos, por causa do TS.
-
-    const props = defineProps({...})
-
-    const props = defineProps({
-      icon: {
-        type: Object as () => iconProps
-      }
-    })
-  */
+*/
 </script>
 
 
--- src/layout/Login/Login.vue
+++ src/layout/Login/Login.vue
@@ -5,21 +5,32 @@
         </div>
         <div class="divInput">
             <p>Endereço de e-mail</p>
-            <InputIcon :icon="iconInput" />
+            <InputIcon :property="iconInput" />
+        </div>
+        <div class="divInput">
+            <p>Endereço de e-mail</p>
+            <InputIcon :property="iconInput2" />
         </div>
     </div>
 </template>
 
 <script setup>
-    import InputIcon from '@/components/Input/InputIcon.vue'
+import InputIcon from '@/components/Input/InputIcon.vue'
 
-    import { reactive } from 'vue'
+import { reactive } from 'vue'
 
 
-    const iconInput = reactive({
-        iconFont: ['fa', 'phone'],
-        labelText: 'Digite seu e-mail'
-    })
+const iconInput = reactive({
+    iconFont: ['fa', 'phone'],
+    labelText: 'Digite seu e-mail',
+    iconColor: "white",
+})
+const iconInput2 = reactive({
+    iconFont: ['fa', 'envelope'],
+    labelText: 'Digite sua senha',
+    iconColor: "white",
+})
+
 
 </script>
 <style scoped src="./Login.scss"></style>
