6c1d8b65c18fddf3e1a85cf292cade328c30bf26
João Vitor Alves Muniz <joao.vitor@akamaetech.com>
2023-04-24 19:17:05 -0300

    dskjads

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button type="button" class="btn"
-    :style="{ 'background-color': props.property?.color, 'width': props.property?.width, 'height': props.property?.heigth }">
+    :style="{ 'background-color': props.property?.background, 'width': props.property?.width, 'height': props.property?.height, 'color': props.property?.color }">
     {{ props.property?.text }}
   </button>
 </template>
@@ -12,9 +12,10 @@ import { defineProps as definePropsVue, PropType } from 'vue';
 
 interface propsBtn {
   text: string | null,
-  color: string | null
-  width: string | null
-  heigth: string | null
+  color: string | null,
+  background: string | null,
+  width: string | null,
+  height: string | null
 }
 
 
--- src/layout/Login/Login.vue
+++ src/layout/Login/Login.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="divMain">
-        <div class="divInput">
-            <div>
-                <h1>Entrar</h1>
+        <div class="divMainInput">
+            <div class="divInput">
+                <div style="display: flex;align-items: center;justify-content: center;">
+                    <img style="width: 500px;" src="./estilodev.png">
+                </div>
+                <div>
+                    <h1>Entrar</h1>
+                </div>
+                <p>Endereço de e-mail</p>
+                <InputIcon :property="iconInputEmail" />
             </div>
-            <p>Endereço de e-mail</p>
-            <InputIcon :property="iconInputEmail" />
-        </div>
-        <div class="divInput">
-            <p>Endereço de e-mail</p>
-            <InputIcon :property="iconInputPassword" />
-            <Button :property="dataButtonAdmin" />
+            <div class="divInput">
+                <p>Endereço de e-mail</p>
+                <InputIcon :property="iconInputPassword" />
+                <div style="margin-top: 40px">
+                    <Button :property="dataButtonAdmin" />
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
@@ -22,10 +30,11 @@ import { reactive } from 'vue'
 
 
 const dataButtonAdmin = reactive({
-    text: 'Login',
-    color: 'red',
+    text: 'Entrar',
+    background: '#ff0030',
+    color: '#ffffff',
     width: '100%',
-    height: '100px'
+    height: '80px'
 })
 const iconInputEmail = reactive({
     iconFont: ['fa', 'phone'],
